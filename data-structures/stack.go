f451d78a846e8bfa7cc65d4073ce8bfdfb91b227
kaiqnes <caique.nunes.ext@jumia.com>
2022-04-14 10:04:18 -0300

    update stack to receive any type of values

--- data-structures/stack.go
+++ data-structures/stack.go
@@ -1,25 +1,27 @@
 package data_structures
 
-type Stack []string
+type Stack struct {
+	values []interface{}
+}
 
 // IsEmpty: check if stack is empty
 func (s *Stack) IsEmpty() bool {
-	return len(*s) == 0
+	return len(s.values) == 0
 }
 
 // Push a new value onto the stack
-func (s *Stack) Push(str string) {
-	*s = append(*s, str) // Simply append the new value to the end of the stack
+func (s *Stack) Push(val interface{}) {
+	s.values = append(s.values, val) // Simply append the new value to the end of the stack
 }
 
 // Remove and return top element of stack. Return false if stack is empty.
-func (s *Stack) Pop() (string, bool) {
+func (s *Stack) Pop() (interface{}, bool) {
 	if s.IsEmpty() {
-		return "", false
+		return nil, false
 	} else {
-		index := len(*s) - 1   // Get the index of the top most element.
-		element := (*s)[index] // Index into the slice and obtain the element.
-		*s = (*s)[:index]      // Remove it from the stack by slicing it off.
+		index := len(s.values) - 1    // Get the index of the top most element.
+		element := (s.values)[index]  // Index into the slice and obtain the element.
+		s.values = (s.values)[:index] // Remove it from the stack by slicing it off.
 		return element, true
 	}
 }
